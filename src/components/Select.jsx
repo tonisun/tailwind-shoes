ffa21174b12bc33d4ba62cafbe3197b65c3ce1d5
Toni Zeidler <toni.z@gmx.de>
2024-03-12 08:11:01 +0100

    add Select

---
+++ src/components/Select.jsx
@@ -0,0 +1,14 @@
+export function Select() {
+
+    return (
+      <dev className="">
+        <select name="" id="">
+            <option value=""></option>
+            <option value=""></option>
+            <option value=""></option>
+            <option value=""></option>
+            <option value=""></option>
+        </select>
+      </dev>
+    )
+  }
